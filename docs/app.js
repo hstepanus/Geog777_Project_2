359a298b80ad5b2148d79c704faf17f1ad3c9d85
hstepanus <hstepanus@yahoo.com>
2026-03-28 23:11:49 -0400

    const map to 16

--- docs/app.js
+++ docs/app.js
@@ -7,7 +7,7 @@ if (!deviceId) {
   localStorage.setItem(deviceIdKey, deviceId);
 }
 
-const map = L.map("map", { zoomControl: false }).setView([38.7660, -77.3070], 14);
+const map = L.map("map", { zoomControl: false }).setView([38.7660, -77.3070], 16);
 L.control.zoom({ position: "bottomright" }).addTo(map);
 
 const lightTiles = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
